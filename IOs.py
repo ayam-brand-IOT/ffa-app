0854c95c4956e1a91e088127f843f78520e35982
Emmanuel Lastra <elastra@uabc.edu.mx>
2024-02-08 15:31:30 -0800

    What's actually at the FFA

--- IOs.py
+++ IOs.py
@@ -9,11 +9,11 @@ for index in range(len(io_map.inputs)):
     pi.set_mode(io_map.inputs[index], pigpio.INPUT)
     pi.set_mode(io_map.outputs[index], pigpio.OUTPUT)
 
-def laser():
+def laser(value):
     global laser_state
 
     laser_state = not laser_state
-    pi.write(io_map.__LASER_PIN, laser_state)
+    pi.write(io_map.__LASER_PIN, value)
 
 def flash(value):
     pi.write(io_map.__FLASH_PIN, value)
